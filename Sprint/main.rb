006263f9a513377999b6c27e67c64ff9b5b3124f
kocjanr <ryankojan@Ryans-MacBook-Pro.local>
2017-02-02 05:58:11 -0800

    Initial commit

---
+++ Sprint/main.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+class MadLib
+
+  def initialize()
+
+  end
+
+  def addToVector(input)
+    puts input
+  end
+
+  def randomize()
+
+  end
+end
+
+object = MadLib.new
+
+puts "Enter an item"
+name =  gets
+
+object.addToVector(name)
